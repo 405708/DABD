fa08874ee603d567c63e2d419b2fb3f67cb101cf
405708 <405708@tecnicatura.frc.utn.edu.ar>
2024-08-30 19:05:23 -0300

    Angular Proyectos

---
+++ Angular Proyectos/chat/src/app/chat-mensajes/chat-mensajes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-mensajes',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-mensajes.component.html',
+  styleUrl: './chat-mensajes.component.css'
+})
+export class ChatMensajesComponent {
+
+}
---
+++ Angular Proyectos/chat/src/app/chat-sala/chat-sala.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-sala',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-sala.component.html',
+  styleUrl: './chat-sala.component.css'
+})
+export class ChatSalaComponent {
+
+}
