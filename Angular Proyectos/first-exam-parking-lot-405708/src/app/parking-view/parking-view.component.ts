7f9dc2274e462b4a5927e22f6adc4eb192302b9e
405708 <405708@tecnicatura.frc.utn.edu.ar>
2024-09-27 01:57:20 -0300

    Actualizacion Parcial

--- Angular Proyectos/first-exam-parking-lot-405708/src/app/parking-view/parking-view.component.ts
+++ Angular Proyectos/first-exam-parking-lot-405708/src/app/parking-view/parking-view.component.ts
@@ -11,14 +11,15 @@ import { PARKING_LOT_COPY, Vehicule, VehiculeType } from '../services/data/mock-
   styleUrl: './parking-view.component.css'
 })
 export class ParkingViewComponent implements OnInit{
-  parkingSlot : any[] = PARKING_LOT_COPY
+  parkingSlot : any[] = []
 
   private parkingService  = inject(ParkingService)
 
 
 
   ngOnInit(): void {
-    this.subscribeParkings()
+    this.subscribeParkings();
+    this.organizeParking();
   }
 
   subscribeParkings(){
@@ -32,6 +33,56 @@ export class ParkingViewComponent implements OnInit{
   }
 
   @Input() vehiculeToAdd: Vehicule[] = []
+
+
+  //Vista parking
+  organizeParking() {
+    const slotsPerRow = 6;
+    this.parkingSlot = [];
+    for (let i = 0; i < PARKING_LOT_COPY.length; i += slotsPerRow) {
+      this.parkingSlot.push(PARKING_LOT_COPY.slice(i, i + slotsPerRow));
+    }
+  }
+
+  getSlotClass(slot: any) {
+    return slot.availablePlaces === 0 ? 'border-danger' : 'border-success';
+  }
+
+  getVehicleClass(vehicle: VehiculeType) {
+    return 'vehicle';
+  }
+
+  getVehicleIcon(vehicle: VehiculeType) {
+    switch (vehicle) {
+      case VehiculeType.CAR:
+        return 'bi bi-car-front-fill';
+      case VehiculeType.MOTORCYCLE:
+        return 'bi bi-bicycle';
+      case VehiculeType.TRUCK:
+        return 'bi bi-truck';
+      default:
+        return '';
+    }
+  }
+
+  getVehicleTypeLabel(vehicle: VehiculeType) {
+    switch (vehicle) {
+      case VehiculeType.CAR:
+        return 'Auto';
+      case VehiculeType.MOTORCYCLE:
+        return 'Moto';
+      case VehiculeType.TRUCK:
+        return 'Camión';
+      default:
+        return '';
+    }
+  }
+
+  createArray(number: number): number[] {
+    return new Array(number);
+  }
+
+
   
 
 }
